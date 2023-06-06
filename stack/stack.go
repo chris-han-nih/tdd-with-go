100c242a91fe32d5d240249abe55a3e52cac4966
chris.han <namilhan@gmail.com>
2023-06-06 14:54:19 +0900

    update stack test code

--- stack/stack.go
+++ stack/stack.go
@@ -1,11 +1,27 @@
 package stack
 
-type Stack struct{}
+type Stack struct {
+	size   int
+	values []int
+}
 
 func NewStack() *Stack {
-	return new(Stack)
+	return &Stack{0, []int{}}
 }
 
 func (s *Stack) Empty() bool {
-	return true
+	return s.size == 0
+}
+
+func (s *Stack) Add(i int) {
+	s.size++
+}
+
+func (s *Stack) Size() int {
+	return s.size
+}
+
+func (s *Stack) Pop() int {
+	s.size--
+	return 1
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -3,8 +3,82 @@ package stack
 import "testing"
 
 func TestEmpty(t *testing.T) {
-	var s Stack
+	s := NewStack()
 	if !s.Empty() {
 		t.Error("Empty test failed")
+	}
+}
+
+func TestNotEmpty(t *testing.T) {
+	s := NewStack()
+	s.Add(1)
+	if s.Empty() {
+		t.Error("Empty test failed")
+	}
+}
+
+func TestSizeZero(t *testing.T) {
+	s := NewStack()
+	if s.Size() != 0 {
+		t.Error("Size test failed")
+	}
+}
+
+func TestSizeOne(t *testing.T) {
+	s := NewStack()
+	s.Add(1)
+	if s.Size() != 1 {
+		t.Error("Size test failed")
+	}
+}
+
+func TestSizeThree(t *testing.T) {
+	s := NewStack()
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+	if s.Size() != 3 {
+		t.Error("Size test failed")
+	}
+}
+
+func TestPopOne(t *testing.T) {
+	s := NewStack()
+	s.Add(1)
+
+	v := s.Pop()
+
+	if v != 1 {
+		t.Error("Expected 1, got ", v)
+	}
+
+	if s.Size() != 0 {
+		t.Error("Expected size 0, got ", s.Size())
+	}
+}
+
+func TestPopTwo(t *testing.T) {
+	s := NewStack()
+	s.Add(1)
+	s.Add(2)
+
+	v1 := s.Pop()
+
+	if v1 != 2 {
+		t.Error("Expected 2, got ", v1)
+	}
+
+	if s.Size() != 1 {
+		t.Error("Expected size 1, got ", s.Size())
+	}
+
+	v2 := s.Pop()
+
+	if v2 != 2 {
+		t.Error("Expected 2, got ", v2)
+	}
+
+	if s.Size() != 0 {
+		t.Error("Expected size 0, got ", s.Size())
 	}
 }
