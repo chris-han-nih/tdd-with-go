c8cd7585e673fa3d27447a3db42ab2614b0a3a17
chris.han <namilhan@gmail.com>
2023-06-08 20:33:54 +0900

    Refactor test code: replace current methods with Testify framework for better readability and maintainability

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Test Driven development with Golang
 ---
 [Youtube](https://www.youtube.com/watch?v=yFknOWRWpoY&list=PLtFquUj7IL8VpSL98BTvl3lnD8HS4NGlA)
 ## Stack
+
+## Install testify
+```bash
+$ go get github.com/stretchr/testify
+```
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,12 +1,34 @@
 package stack
 
-import "testing"
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type StackSuite struct {
+	suite.Suite
+}
+
+func TestStackSuite(t *testing.T) {
+	suite.Run(t, new(StackSuite))
+}
+
+func (s *StackSuite) TestEmpty() {
+	stack := NewStack()
+	s.True(stack.Empty())
+}
 
 func TestEmpty(t *testing.T) {
 	s := NewStack()
 	if !s.Empty() {
 		t.Error("Empty test failed")
 	}
+}
+
+func (s *StackSuite) TestNotEmpty() {
+	stack := NewStack()
+	stack.Add(1)
+	s.False(stack.Empty())
 }
 
 func TestNotEmpty(t *testing.T) {
@@ -17,11 +39,22 @@ func TestNotEmpty(t *testing.T) {
 	}
 }
 
+func (s *StackSuite) TestSizeZero() {
+	stack := NewStack()
+	s.Equal(0, stack.Size())
+}
+
 func TestSizeZero(t *testing.T) {
 	s := NewStack()
 	if s.Size() != 0 {
 		t.Error("Size test failed")
 	}
+}
+
+func (s *StackSuite) TestSizeOne() {
+	stack := NewStack()
+	stack.Add(1)
+	s.Equal(1, stack.Size())
 }
 
 func TestSizeOne(t *testing.T) {
@@ -32,6 +65,13 @@ func TestSizeOne(t *testing.T) {
 	}
 }
 
+func (s *StackSuite) TestSizeTwo() {
+	stack := NewStack()
+	stack.Add(1)
+	stack.Add(2)
+	s.Equal(2, stack.Size())
+}
+
 func TestSizeThree(t *testing.T) {
 	s := NewStack()
 	s.Add(1)
@@ -40,6 +80,15 @@ func TestSizeThree(t *testing.T) {
 	if s.Size() != 3 {
 		t.Error("Size test failed")
 	}
+}
+
+func (s *StackSuite) TestPopOne() {
+	stack := NewStack()
+	stack.Add(1)
+
+	v := stack.Pop()
+
+	s.Equal(1, v)
 }
 
 func TestPopOne(t *testing.T) {
@@ -55,6 +104,24 @@ func TestPopOne(t *testing.T) {
 	if s.Size() != 0 {
 		t.Error("Expected size 0, got ", s.Size())
 	}
+}
+
+func (s *StackSuite) TestPopTwo() {
+	stack := NewStack()
+	stack.Add(1)
+	stack.Add(2)
+
+	v1 := stack.Pop()
+
+	s.Equal(2, v1)
+
+	s.Equal(1, stack.Size())
+
+	v2 := stack.Pop()
+
+	s.Equal(1, v2)
+
+	s.Equal(0, stack.Size())
 }
 
 func TestPopTwo(t *testing.T) {
